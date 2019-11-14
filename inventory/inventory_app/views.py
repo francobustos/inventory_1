c1dba475bb88fcc650c7e8159afdcefbc3c1eb27
FranMartiarena <franciscomartiarena@gmail.com>
2019-11-14 11:54:50 -0300

    cree html de forms del objeto

--- inventory/inventory_app/views.py
+++ inventory/inventory_app/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from inventory_app.models import *
 from django.utils import timezone
 
+
 def post_list(request):
     area = Area.objects.all()
     container = Container.objects.all()
     objeto = Objeto.objects.all()
     return render(request, 'index.html', {'area':area, 'container':container, 'objeto':objeto})
+
+def objeto(request):
+    objeto = Objeto.objects.all()
+    return render(request, 'objetos.html',{'objeto':objeto})
