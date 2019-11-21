1b0c0b298bfb9435f43c95d6d91ba0eeaa955e23
Nicanor Diaz <diaz.nicanor123@gmail.com>
2019-11-21 09:59:47 -0300

    crear_area y editar_area

--- inventory/inventory_app/forms.py
+++ inventory/inventory_app/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Container
+from .models import Container,Area
 
 class ContainerForm(forms.ModelForm):
     class Meta:
         model = Container
         fields = ['ID_profe','nombre','area_de_origen']
+
+class AreaForm(forms.ModelForm):
+	class Meta:
+		model = Area
+		fields = ['nombre']
