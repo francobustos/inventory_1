25156396fcf1bdbf34f3320632c2aec390cb44a2
francobustos <franco.bustos60@gmail.com>
2019-11-21 08:40:02 -0300

    Explique en views unos comandos

--- inventory/inventory_app/views.py
+++ inventory/inventory_app/views.py
@@ -26,11 +26,11 @@ def my_login(request):
         return render(request, 'login.html')
 
 def crear_container(request):
-    if request.method == 'POST':
-        container_form = ContainerForm(request.POST)
-        if container_form.is_valid():
-            container_form.save()
-            return redirect('post_list')
-    else:
-        container_form = ContainerForm()
-    return render(request, 'edicion.html', {'form':container_form,'titulo':"Crear container"})
+    if request.method == 'POST': #Para ver si la peticion de la pagina quiere enviar algo al servidor
+        container_form = ContainerForm(request.POST) #Crea una variable con la peticion que le envia la pagina y lo almacena como un formulario
+        if container_form.is_valid(): #Verifica si todos los campos estan bien con respecto a la base de datos
+            container_form.save() #Guarda el formulario en la base de datos
+            return redirect('post_list') #Redirecciona al index
+    else: #Para ver si la peticion de la pagina quiere traer algo del servidor a la pagina
+        container_form = ContainerForm() #Crea un formulario vacio
+    return render(request, 'edicion.html', {'form':container_form,'titulo':"Crear container"}) #Renderiza la pagina edicion.html y le envia un formulario y un string
